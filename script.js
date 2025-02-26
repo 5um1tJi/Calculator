4ebf89a05ca6fa6c2544de115d70319fbc01262d
Sumit Mishra <sumasylum@gmail.com>
2025-02-26 22:52:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const display = document.getElementById("display");
+function append(input){
+    display.value += input;
+}
+
+function Clear(){
+    display.value = "";
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Logic Error Bro !";
+        setTimeout(() => {
+            console.log("Executing next code...");
+            display.value = "";
+        }, 1000);
+    }
+
+}
+function backspace(){
+    display.value = display.value.slice(0,-1);
+}
